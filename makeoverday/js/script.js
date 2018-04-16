5d4886640fed0de3eb9784ee818a89f0deb8c34b
Rodrigo Gabriel <Rodrigo@gmail.com>
2018-04-16 13:18:49 -0300

    Finalizando o site

---
+++ makeoverday/js/script.js
@@ -0,0 +1,34 @@
+ $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+        $('.go-top').fadeIn(200);
+        } else {
+          $('.go-top').fadeOut(200);
+        }
+        });   
+        // Animate the scroll to top
+      $('.go-top').click(function(event) {
+        event.preventDefault();
+      $('html, body').animate({scrollTop: 0}, 300);
+      })
+
+
+/*Menu*/
+        $(function(){   
+          
+          var superbg = $('.super');  
+          var nav = $('nav');
+ 					
+					nav.hide();
+					
+          $(window).scroll(function () { 
+            if ($(this).scrollTop() >= 300) {
+							nav.show();
+							
+            } else {  
+							nav.hide();
+							superbg.show();
+            } 
+          });  
+          
+        });
+/*Menu*/
